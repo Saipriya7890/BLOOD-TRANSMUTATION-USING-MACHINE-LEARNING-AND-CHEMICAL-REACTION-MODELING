adc51cd020ee454e13d885853626f4b4c1008f2e
Saipriya Chilukuri <152782660+Saipriya7890@users.noreply.github.com>
2024-12-07 13:57:25 +0530

    Add files via upload

---
+++ port.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+var (
+  PORT string
+)
+
+func init() {
+  PORT = os.Getenv("PORT")
+  if PORT == "" {
+    log.Println("PORT not found, using default value")
+    PORT = "8080"
+  }
+}
+
+func GetPort() string {
+  return PORT
+}
